2086f9d57592ba1990009c3344ad99ab601c8266
Zach <zachwinger.alt@gmail.com>
2022-08-19 10:42:30 -0400

    Add shading on mouseover to grid

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 const container = document.querySelector('#container');
+let size = 16;
+let width = 500/size;
 
-for (let i = 0; i < (16 * 16); i++){
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+    sizeString = prompt("Enter a Size");
+    size = parseInt(sizeString);
+    console.log(size);
+})
+
+for (let i = 0; i < (size * size); i++){
     const content = document.createElement('div');
     content.classList.add('content');
     //content.textContent = i;
     container.appendChild(content);
 }
 
-let size = 16;
-let width = 500/size;
-
 container.style.cssText = `grid-template-columns: repeat(${size}, ${width}px); grid-template-rows: repeat(${size}, ${width}px);`
+
+const squares = document.querySelectorAll('.content');
+
+squares.forEach((square) => {
+    square.addEventListener('mouseenter', () => {
+        square.classList.add('shading');
+    });
+});
